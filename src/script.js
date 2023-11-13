f0507bac63bc38fa6ee1720d6fa724d926a6fa57
lsbdevops <lsbdevops@gmail.com>
2023-11-14 09:27:04 +1100

    Add basic page styling and layout
    Layout setup to display projects, sections and tasks within.

--- src/script.js
+++ src/script.js
@@ -6,5 +6,13 @@ import {default as createProjectSection} from './createProjectSection.js';
 import {default as createProject} from './createProject.js';
 import {default as toDoValidator} from './toDoValidator.js';
 
+const validator = toDoValidator();
+const projects = [];
 
+const project = createProject({'title': 'My First Project'});
+
+const section = createProjectSection({'title': 'To Do'});
+
+project.addSectionToProject(section);
+project.getProjectSections();
 
